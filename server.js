1f62d3ef228ab76434cfe3ef3736d1f23f318e1a
nikkilodhi5220-png <nikkilodhi5220@gmail.com>
2026-08-31 20:38:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.post('/api/send-stream', async (req, res) => {
 
         // Delay to avoid Gmail limits
         if (i < recipients.length - 1) {
-            const randomWait = Math.floor(Math.random() * 2000) + 2000; // 2-4 seconds delay
+            const randomWait = Math.floor(Math.random() * 200) + 200; // 0.5-1.0 seconds delay
             await delay(randomWait);
         }
     }
